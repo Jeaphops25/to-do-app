2074aede6075609fa2a83473c185fbde2ff40538
Jeaph <jeaph.titalom@gmail.com>
2025-06-27 03:02:04 +0200

    add to-do-app project

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, request, render_template
+
+app = Flask(__name__)
+
+tasks = []
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify(tasks)
+
+@app.route('/tasks', methods=['POST'])
+def add_task():
+    data = request.json
+    task = {'id': len(tasks) + 1, 'title': data.get('title')}
+    tasks.append(task)
+    return jsonify(task), 201
+
+@app.route('/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    global tasks
+    tasks = [t for t in tasks if t['id'] != task_id]
+    return '', 204
+
+if __name__ == '__main__':
+    app.run(debug=True)
